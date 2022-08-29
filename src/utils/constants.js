28cab0e4711113201eaed725956e2031aad6a97f
Constantin Vanchurenkov <i@wacorasu.ru>
2022-08-29 22:57:06 +0300

    Базовое отображение наград

---
+++ src/utils/constants.js
@@ -0,0 +1,50 @@
+import rewardImage01 from '../images/rewards/reward_01.jpg' 
+import rewardImage02 from '../images/rewards/reward_02.jpg' 
+import rewardImage03 from '../images/rewards/reward_03.jpg' 
+import rewardImage04 from '../images/rewards/reward_04.jpg' 
+import rewardImage05 from '../images/rewards/reward_05.jpg' 
+import rewardImage06 from '../images/rewards/reward_06.jpg' 
+import rewardImage07 from '../images/rewards/reward_07.jpg' 
+import rewardImage08 from '../images/rewards/reward_08.jpg' 
+import rewardImage09 from '../images/rewards/reward_09.jpg' 
+
+export {listOfRewards}
+
+const listOfRewards = [
+    { name: 'Кубок ххх г.',
+      image: rewardImage01,
+      description: 'Блаблаблбалаблаблабалбалбала'
+    },
+    { name: 'Кубок ххх г.',
+      image: rewardImage02,
+      description: 'Блаблаблбалаблаблабалбалбала'
+    },
+    { name: 'Кубок ххх г.',
+      image: rewardImage03,
+      description: 'Блаблаблбалаблаблабалбалбала'
+    },
+    { name: 'Кубок ххх г.',
+      image: rewardImage04,
+      description: 'Блаблаблбалаблаблабалбалбала'
+    },
+    { name: 'Кубок ххх г.',
+      image: rewardImage05,
+      description: 'Блаблаблбалаблаблабалбалбала'
+    },
+    { name: 'Кубок ххх г.',
+      image: rewardImage06,
+      description: 'Блаблаблбалаблаблабалбалбала'
+    },
+    { name: 'Кубок ххх г.',
+      image: rewardImage07,
+      description: 'Блаблаблбалаблаблабалбалбала'
+    },
+    { name: 'Кубок ххх г.',
+      image: rewardImage08,
+      description: 'Блаблаблбалаблаблабалбалбала'
+    },
+    { name: 'Кубок ххх г.',
+      image: rewardImage09,
+      description: 'Блаблаблбалаблаблабалбалбала'
+    }
+]
